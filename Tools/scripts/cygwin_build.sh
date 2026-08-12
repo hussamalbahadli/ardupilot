064e12baa0f9a9d485dbaa8309e4319cf3322fbd
hussamalbahadli <hussamalbahadli@gmail.com>
2026-08-12 20:36:47 +0300

    Fix compass issue not change orenation on built in board and add copter simulation

--- Tools/scripts/cygwin_build.sh
+++ Tools/scripts/cygwin_build.sh
@@ -25,7 +25,7 @@ mkdir artifacts
 (
     python ./waf --color yes --toolchain $TOOLCHAIN --board sitl configure 2>&1
     python ./waf plane 2>&1
-#    python ./waf copter 2>&1
+    python ./waf copter 2>&1
 #    python ./waf heli 2>&1
 #    python ./waf rover 2>&1
 #    python ./waf sub 2>&1
@@ -34,13 +34,13 @@ mkdir artifacts
 # copy both with exe and without to cope with differences
 # between windows versions in CI
 cp -v build/sitl/bin/arduplane artifacts/ArduPlane.elf.exe
-#cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf.exe
+cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf.exe
 #cp -v build/sitl/bin/arducopter-heli artifacts/ArduHeli.elf.exe
 #cp -v build/sitl/bin/ardurover artifacts/ArduRover.elf.exe
 #cp -v build/sitl/bin/ardusub artifacts/ArduSub.elf.exe
 
 cp -v build/sitl/bin/arduplane artifacts/ArduPlane.elf
-#cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf
+cp -v build/sitl/bin/arducopter artifacts/ArduCopter.elf
 #cp -v build/sitl/bin/arducopter-heli artifacts/ArduHeli.elf
 #cp -v build/sitl/bin/ardurover artifacts/ArduRover.elf
 #cp -v build/sitl/bin/ardusub artifacts/ArduSub.elf
